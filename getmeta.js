16f4efc37dcf3d9124b5165dd6170e994681935c
Joe Sutherland <1004421@rgu.ac.uk>
2015-09-30 16:04:17 +0100

    +

--- getmeta.js
+++ getmeta.js
@@ -13,6 +13,7 @@ $(function () {
         website = new RegExp("www."),
         trim = new RegExp("\/.*"),
         i = 0,
+        colours = ["RGBA(160, 0, 46, 0.3)", "RGBA(231, 163, 58, 0.3)", "RGBA(244, 202, 88, 0.3)", "RGBA(167, 194, 68, 0.3)", "RGBA(68, 131, 146, 0.3)", "RGBA(158, 184, 254, 0.3)", "RGBA(231, 154, 255, 0.3)", "RGBA(186, 94, 232, 0.3)", "RGBA(67, 40, 187, 0.3)"],
         now = new Date(),
         monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
         dayNumbers = [31, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30],
@@ -40,12 +41,20 @@ $(function () {
             }
         });
         $('.toggle').remove();
-        $('.meta').css({"background": "white", "border": "none"});
+        $('li.meta').css({"background": "white", "border": "none", "padding": "0"});
+        $('li.date').css({"background": "white", "border": "none", "padding": "0"});
+        $('.sectiontitle').each(function () {
+            var sectionTitle = $(this).html();
+            console.log(sectionTitle);
+            if (sectionTitle == "SECTIONTITLE<br>") {
+                alert("Double-check your section titles!")
+            }
+        });
     });
     
     $('#add-date').click(function () {
         var newDate = $('#date-entry').val();
-        $('#resultlist').append("<li class='date'>" + newDate + "<br/><br/></li>");
+        $('#resultlist').append("<li class='date'><p>" + newDate + "</p></li>");
     });
     
     $('#help').click(function () {
@@ -65,19 +74,23 @@ $(function () {
         $('#header').append(monthNames[now.getMonth()] + " " + now.getDate() + ", " + now.getFullYear());
         var descriptionText = $('textarea[name="desc"]').val();
         $('#description').append("In today's report...<br/><br/>");
-        $('#resultlist').append("<li class='date'>" + yesterday + "<br/><br/></li>");
+        $('#resultlist').append("<li class='date'><p>" + yesterday + "</p></li>");
         
 		$($('textarea[name="urls"]').val().splitNewline())
             .each(function () {
                 var fetchURL = this,
                     newURL = fetchURL.replace(reHTTP, ""),
-                    websiteName = newURL.replace(website, "").replace(trim, "");
+                    websiteName = newURL.replace(website, "").replace(trim, ""),
+                    randomColor = colours[i];
+                i = i + 1;
+                if (i === 9) {
+                    i = 0;
+                }
                 $.ajax({
                     url: "http://textance.herokuapp.com/title/" + encodeURIComponent(newURL),
                     complete: function (data) {
                         var title = data.responseText;
-                        $('#resultlist').append("<li id='section" + i + "' class='meta'><span class='sectiontitle contenteditable' contenteditable='true'>SECTIONTITLE<br/></span><span class='related hidden'>Related Stories:<br/></span><span class='entry contenteditable'  contenteditable='true'>" + websiteName + " - " + data.responseText + "<br/></span>" + fetchURL + "<a class='toggle'> &bull; toggle</a><br /><br /></li>");
-                        i = i + 1;
+                        $('#resultlist').append("<li class='meta' style='background:" + randomColor + "'><span class='sectiontitle contenteditable' contenteditable='true'>SECTIONTITLE<br/></span><span class='related hidden'>Related Stories:<br/></span><span class='entry contenteditable'  contenteditable='true'>" + websiteName + " - " + data.responseText + "<br/></span>" + fetchURL + "<a class='toggle'> &bull; toggle</a><br /><p class='contenteditable quote hidden' contenteditable='true'>QUOTE <a class='removequote'> &bull; remove</a></p></li>");
                     }
                 });
             });
